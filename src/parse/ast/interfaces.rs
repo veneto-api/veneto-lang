053987b669ef61f69f1bbf1b4ba5407af13fad7e
Jacob Conley <jake@jakeconley.com>
2023-02-27 20:43:11 -0500

    Fixed interface tests

--- src/parse/ast/interfaces.rs
+++ src/parse/ast/interfaces.rs
@@ -1,8 +1,8 @@
-use std::str::FromStr;
+use std::{str::FromStr, backtrace::Backtrace};
 
 use strum_macros::{ Display, EnumString };
 
-use crate::parse::{lexer::TokenStream, ParseResult, tokens::{Punctuation, Terminal}, ParseErrorKind};
+use crate::parse::{lexer::{TokenStream, Span}, ParseResult, tokens::{Punctuation, Terminal, Token, Identifier}, ParseErrorKind, ParseError};
 
 use super::{Peekable, Expectable}; 
 
@@ -22,15 +22,21 @@ pub struct InterfaceField {
 
     /// True if the field is marked as optional with a `?` before the `:`. 
     pub optional: bool, 
+
+    pub span: Span, 
 }
 
 /// The body of an Interface literal
-pub type InterfaceBody = Vec<InterfaceField>; 
+#[derive(Debug, PartialEq, Eq)]
+pub struct InterfaceBody {
+    fields: Vec<InterfaceField>, 
+    span: Span, 
+}
 
 /// Either an interface literal or an identifier referencing another interface
 #[derive(Debug, PartialEq, Eq)]
 pub enum InterfaceExpression { 
-    Identifier(String), 
+    Identifier(Identifier), 
     Literal(InterfaceBody), 
 }
 impl Expectable for InterfaceExpression { 
@@ -45,27 +51,38 @@ impl Expectable for InterfaceExpression {
 impl super::Finishable for InterfaceBody { 
     const INITIAL_TOKEN: Terminal = Terminal::Punctuation(Punctuation::BraceOpen);
 
-    fn parse_finish(stream: &mut TokenStream) -> ParseResult<Self> {
-        let mut fields = Self::new(); 
+    fn parse_finish(stream: &mut TokenStream, initial: Token) -> ParseResult<Self> {
+        let mut fields = Vec::<InterfaceField>::new(); 
         loop { 
             if let Some(name) = stream.peek_for_identifier()? { 
                 let optional = stream.peek_for_punctuation(Punctuation::Optional)?; 
                 stream.next()?.expect_punctuation(Punctuation::Colon)?; 
 
-                let typ = stream.next()?;
-                let typ = typ
-                    .try_as_identifier()
-                    .and_then(|s| 
-                        InterfaceValueType::from_str(&s).map_err(|_| typ.as_err(ParseErrorKind::UnknownInterfaceValueType))
-                    )?;
+                let typ = stream.next()?.try_as_identifier()?;
+                let typ_span = typ.span; 
+                let Ok(typ) = InterfaceValueType::from_str(&typ.text) else { 
+                    return Err(ParseError{ 
+                        kind: ParseErrorKind::UnknownInterfaceValueType, 
+                        span: typ_span, 
+                        backtrace: Backtrace::capture(), 
+                    })
+                };
 
-                fields.push(InterfaceField { name, typ, optional });
+                fields.push(InterfaceField { 
+                    name: name.text, 
+                    typ, 
+                    optional,
+                    span: name.span.through(typ_span),
+                });
             }
 
             let next = stream.next()?; 
             match next.as_punctuation() { 
                 Some(Punctuation::Comma) => continue, 
-                Some(Punctuation::BraceClose) => return Ok(fields), 
+                Some(Punctuation::BraceClose) => return Ok(InterfaceBody { 
+                    fields, 
+                    span: initial.span.through(next.span),
+                }), 
                 _ => return Err(next.as_err_unexpected())
     ,        }
         }
@@ -77,47 +94,52 @@ impl super::Finishable for InterfaceBody {
 mod test {
     use crate::parse::{ParseResult, lexer::TokenStream, ast::Expectable};
 
-    use super::{InterfaceBody, InterfaceField, InterfaceValueType};
+    use super::InterfaceBody;
 
     fn parse_interface(input: &str) -> ParseResult<InterfaceBody> { 
         let mut stream = TokenStream::new(input.chars()); 
         InterfaceBody::parse_expect(&mut stream)
     }
 
-    fn assert_interface(input: &str, expected: InterfaceBody) { 
+    fn assert_interface(input: &str, expected: &[(&str, bool)]) { 
         let i = parse_interface(input).unwrap(); 
-        assert_eq!(i, expected);
-    }
+        let mut fields = i.fields.iter(); 
 
-    fn make_ifield(name: &'static str, optional: bool ) -> InterfaceField { 
-        InterfaceField { name: name.to_string(), typ: InterfaceValueType::String, optional }
+        for pair in expected { 
+            let field = fields.next().unwrap(); 
+            assert_eq!(pair.0, field.name);
+            assert_eq!(pair.1, field.optional);
+        }
+
+        assert_eq!(fields.next(), None); 
     }
 
     #[test]
     fn empty() { 
-        assert_interface("{}", vec![]);
+        let interface = parse_interface("{}").unwrap();
+        assert!(interface.fields.is_empty()); 
     }
 
     #[test]
     fn simple() { 
-        assert_interface("{ foo: string }", vec![ 
-            make_ifield("foo", false)
+        assert_interface("{ foo: string }", &[ 
+            ("foo", false)
         ])
     }
 
     #[test]
     fn optional() { 
-        assert_interface("{  asdf? : string }", vec![ 
-            make_ifield("asdf", true)
+        assert_interface("{  asdf? : string }", &[ 
+            ("asdf", true)
         ])
     }
 
     #[test]
     fn complex() { 
-        assert_interface(" {foo: string  , bar?: string, baz  : string}", vec![ 
-            make_ifield("foo", false),
-            make_ifield("bar", true),
-            make_ifield("baz", false),
+        assert_interface(" {foo: string  , bar?: string, baz  : string}", &[ 
+            ("foo", false),
+            ("bar", true),
+            ("baz", false),
         ])
     }
 
